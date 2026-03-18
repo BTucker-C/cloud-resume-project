353c265fbcd7800bec0cdbb50062165a5f74b6f2
Brandon Tucker <brandontucker@comcast.net>
2026-03-18 13:38:46 -0400

    Align Terraform with existing AWS resources.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-2"
+  region = var.region
 }
 
 resource "aws_dynamodb_table" "visitor_count" {
-  name         = "resume-visitor-count"
+  name         = "resume-counter"
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "id"
@@ -15,31 +15,13 @@ resource "aws_dynamodb_table" "visitor_count" {
 }
 
 resource "aws_lambda_function" "visitor_counter" {
-  function_name = "resume-visitor-counter"
-  role          = aws_iam_role.lambda_role.arn
+  function_name = "resume-counter-function"
+  role          = "arn:aws:iam::131233419733:role/service-role/resume-counter-function-role-cgnu3sav"
   handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.12"
+  runtime       = "python3.14"
 
   filename         = "lambda.zip"
   source_code_hash = filebase64sha256("lambda.zip")
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "resume-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
-}
+variable "region" {}
