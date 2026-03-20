79d8dcd7587388e4041edeb927977522fcac38a7
Brandon Tucker <brandontucker@comcast.net>
2026-03-20 13:39:29 -0400

    Correcting script.js.

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 async function updateCounter() {
     try {
-        const response = await fetch("https://5oyiwm4qk0.execute-api.us-east-2.amazonaws.com/prod/count");
+        const response = await fetch("https://5oyiwm4qk0.execute-api.us-east-2.amazonaws.com/count");
         const data = await response.json();
         document.getElementById("counter").innerText = data.count;
     } catch (error) {
